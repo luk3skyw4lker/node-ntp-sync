7179eee16dc57666c150db1c5a5793989fafe5e9
luk3skyw4lker <lucashenriqueblemos@gmail.com>
2020-08-25 04:23:31 -0300

    Adding tests to client

---
+++ __tests__/index.spec.js
@@ -0,0 +1,12 @@
+const Client = require("../index").Client;
+
+test("Sync time", async () => {
+	const NTPClient = new Client();
+	const date = new Date();
+
+	const { time } = await NTPClient.syncTime();
+
+	expect(time.getDay()).toEqual(date.getDay());
+	expect(time.getHours()).toEqual(date.getHours());
+	expect(time.getMinutes()).toEqual(date.getMinutes());
+});
--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,25 @@
-import Packet from "./src/packet";
+export interface NTPPacket {
+	leapIndicator: number;
+	version: number;
+	mode: number;
+	stratum: number;
+	pollInterval: number;
+	precision: number;
+	referenceIdentifier: Buffer;
+	referenceTimestamp: number;
+	originateTimestamp: number;
+	receiveTimestamp: number;
+	transmitTimestamp: number;
+	rootDelay: Buffer;
+	rootDispersion: Buffer;
+	destinationTimestamp: number;
+	time: Date;
+	d: number;
+	t: number;
+}
 
 declare class Client {
-	syncTime(): Promise<Packet>;
+	syncTime(): Promise<NTPPacket>;
 }
 
 declare class Server {
