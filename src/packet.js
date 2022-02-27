9e13a9fb722b539af8f4491d78199ab969bdbbeb
luk3skyw4lker <lucashenriqueblemos@gmail.com>
2022-02-26 22:28:06 -0300

    [refactor]: Rewrite packet to conform with SNTP RFC

--- src/packet.js
+++ src/packet.js
@@ -1,171 +1,7 @@
-const assert = require('assert');
-/**
-                      1                   2                   3
- 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-|LI | VN  |Mode |    Stratum    |     Poll      |   Precision   |
-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-|                          Root Delay                           |
-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-|                       Root Dispersion                         |
-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-|                     Reference Identifier                      |
-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-|                                                               |
-|                   Reference Timestamp (64)                    |
-|                                                               |
-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-|                                                               |
-|                   Originate Timestamp (64)                    |
-|                                                               |
-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-|                                                               |
-|                    Receive Timestamp (64)                     |
-|                                                               |
-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-|                                                               |
-|                    Transmit Timestamp (64)                    |
-|                                                               |
-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-|                 Key Identifier (optional) (32)                |
-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-|                                                               |
-|                                                               |
-|                 Message Digest (optional) (128)               |
-|                                                               |
-|                                                               |
-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
- */
-
-/**
- * 1900 ~ 1970
- * @docs https://tools.ietf.org/html/rfc4330#section-3
- */
-const SEVENTY_YEARS = 2208988800;
-
-/**
- * @rfc https://tools.ietf.org/html/rfc4330
- */
-class Packet {
-	constructor() {
-		Object.assign(this, {
-			leapIndicator: 0,
-			version: 4,
-			mode: 3,
-			stratum: 0,
-			pollInterval: 6,
-			precision: 236,
-			referenceIdentifier: 0,
-			referenceTimestamp: 0,
-			originateTimestamp: 0,
-			receiveTimestamp: 0,
-			transmitTimestamp: 0
-		});
-	}
-
-	static parse(buffer) {
-		assert.equal(buffer.length, 48, 'Invalid Package');
-		const packet = new Packet();
-		packet.leapIndicator = buffer[0] >> 6;
-		packet.version = (buffer[0] & 0x38) >> 3;
-		packet.mode = buffer[0] & 0x7;
-		packet.stratum = buffer[1];
-		packet.pollInterval = buffer[2];
-		packet.precision = buffer[3];
-		packet.rootDelay = buffer.slice(4, 8);
-		packet.rootDispersion = buffer.slice(8, 12);
-		packet.referenceIdentifier = buffer.slice(12, 16);
-		packet.referenceTimestamp = toMsecs(buffer, 16);
-		packet.originateTimestamp = toMsecs(buffer, 24);
-		packet.receiveTimestamp = toMsecs(buffer, 32);
-		packet.transmitTimestamp = toMsecs(buffer, 40);
-		return packet;
-	}
-
-	toBuffer() {
-		const buffer = Buffer.alloc(48).fill(0x00);
-		buffer[0] = 0; // 0b11100011; // LI, Version, Mode
-		buffer[0] += this.leapIndicator << 6;
-		buffer[0] += this.version << 3;
-		buffer[0] += this.mode << 0;
-		buffer[1] = this.stratum;
-		buffer[2] = this.pollInterval;
-		buffer[3] = this.precision;
-		buffer.writeUInt32BE(this.rootDelay, 4);
-		buffer.writeUInt32BE(this.rootDispersion, 8);
-		buffer.writeUInt32BE(this.referenceIdentifier, 12);
-		writeMsecs(buffer, 16, this.referenceTimestamp);
-		writeMsecs(buffer, 24, this.originateTimestamp);
-		writeMsecs(buffer, 32, this.receiveTimestamp);
-		writeMsecs(buffer, 40, this.transmitTimestamp);
-		return buffer;
-	}
-
-	toJSON() {
-		const output = Object.assign({}, this);
-		const { leapIndicator, version } = this;
-		output.version = version;
-		output.leapIndicator = {
-			0: 'no-warning',
-			1: 'last-minute-61',
-			2: 'last-minute-59',
-			3: 'alarm'
-		}[leapIndicator];
-
-		const { mode } = this;
-		switch (mode) {
-			case 1:
-				output.mode = 'symmetric-active';
-				break;
-			case 2:
-				output.mode = 'symmetric-passive';
-				break;
-			case 3:
-				output.mode = 'client';
-				break;
-			case 4:
-				output.mode = 'server';
-				break;
-			case 5:
-				output.mode = 'broadcast';
-				break;
-			case 0:
-			case 6:
-			case 7:
-				output.mode = 'reserved';
-				break;
-		}
-
-		const { stratum } = this;
-		if (stratum === 0) {
-			output.stratum = 'death';
-		} else if (stratum === 1) {
-			output.stratum = 'primary';
-		} else if (stratum <= 15) {
-			output.stratum = 'secondary';
-		} else {
-			output.stratum = 'reserved';
-		}
-
-		output.referenceTimestamp = new Date(this.referenceTimestamp);
-		output.originateTimestamp = new Date(this.originateTimestamp);
-		output.receiveTimestamp = new Date(this.receiveTimestamp);
-		output.transmitTimestamp = new Date(this.transmitTimestamp);
-		output.destinationTimestamp = new Date(this.destinationTimestamp);
-		return output;
-	}
-}
-// 1                   2                   3
-// 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
-// +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-// |                           Seconds                             |
-// +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-// |                  Seconds Fraction (0-padded)                  |
-// +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-
 function toMsecs(buffer, offset) {
 	let seconds = 0;
 	let fraction = 0;
+
 	for (let i = 0; i < 4; ++i) {
 		seconds = seconds * 256 + buffer[offset + i];
 	}
@@ -174,39 +10,130 @@ function toMsecs(buffer, offset) {
 		fraction = fraction * 256 + buffer[offset + i];
 	}
 
-	return (seconds - SEVENTY_YEARS + fraction / Math.pow(2, 32)) * 1000;
+	return seconds + fraction / Math.pow(2, 32);
 }
 
-function writeMsecs(buffer, offset, ts) {
-	// const buffer = Buffer.alloc(8); // 64bits
-	const seconds = Math.floor(ts / 1000) + SEVENTY_YEARS;
-	const fraction = Math.round(((ts % 1000) / 1000) * Math.pow(2, 32));
+const toFrac = ts => {
+	return Math.floor(Math.abs(ts - Math.floor(ts)) * Math.pow(2, 32));
+};
+
+const sysToNTP = timestamp => timestamp + NTPPacket.NTP_DELTA;
+
+const writeInMillis = (buffer, offset, ts, addDelta) => {
+	const seconds = addDelta ? ts + NTP_DELTA : ts;
+	const fraction = toFrac(ts);
+
 	// seconds
 	buffer[offset + 0] = (seconds & 0xff000000) >> 24;
 	buffer[offset + 1] = (seconds & 0x00ff0000) >> 16;
 	buffer[offset + 2] = (seconds & 0x0000ff00) >> 8;
 	buffer[offset + 3] = seconds & 0x000000ff;
+
 	// fraction
 	buffer[offset + 4] = (fraction & 0xff000000) >> 24;
 	buffer[offset + 5] = (fraction & 0x00ff0000) >> 16;
 	buffer[offset + 6] = (fraction & 0x0000ff00) >> 8;
 	buffer[offset + 7] = fraction & 0x000000ff;
-	return buffer;
-}
 
-// Mode     Meaning
-// ------------------------------------
-// 0        reserved
-// 1        symmetric active
-// 2        symmetric passive
-// 3        client
-// 4        server
-// 5        broadcast
-// 6        reserved for NTP control message
-// 7        reserved for private use
-Packet.MODES = {
+	return buffer;
+};
+
+const MODES = {
 	CLIENT: 3,
 	SERVER: 4
 };
 
-module.exports = Packet;
+const NTP_DELTA = 2208988800;
+
+class NTPPacket {
+	constructor(mode) {
+		Object.assign(this, {
+			mode: mode || 4,
+			leap: 0,
+			version: 3,
+			stratum: 0,
+			poll: 0,
+			precision: 0,
+			rootDelay: 0,
+			rootDispersion: 0,
+			referenceId: 0,
+			referenceTimestamp: 0,
+			originateTimestamp: 0,
+			rxTimestamp: 0,
+			txTimestamp: 0
+		});
+	}
+
+	static parse(data) {
+		if (data.length < 48) {
+			throw new Error('Invalid NTP Package');
+		}
+
+		const packet = new NTPPacket(4);
+
+		// Control bytes
+		packet.leap = (data[0] >> 6) & 0x3;
+		packet.version = (data[0] >> 3) & 0x7;
+		packet.mode = data[0] & 0x7;
+		packet.stratum = parseInt(data[1]) || 2;
+		packet.poll = parseInt(data[2]) || 10;
+		packet.precision = parseInt(data[3]);
+		packet.rootDelay = data.slice(4, 8).readFloatBE(0) / 2 ** 16;
+		packet.rootDispersion = data.slice(8, 12).readFloatBE(0) / 2 ** 16;
+		packet.referenceId = data.slice(12, 16);
+
+		// Timestamps where the 4 first bytes are the
+		// int part and the 4 last are the frac part
+		// const refTimestampHigh = data.slice(16, 20).readUint32BE();
+		// const refTimestampLow = data.slice(20, 24).readFloatBE();
+		packet.referenceTimestamp = toMsecs(data, 16);
+
+		// const origTimestampHigh = data.slice(24, 28).readUint32BE();
+		// const origTimestampLow = data.slice(28, 32).readUint32BE();
+		packet.originateTimestamp = toMsecs(data, 24);
+
+		// const rxTimestampHigh = data.slice(32, 36).readUint32BE();
+		// const rxTimestampLow = data.slice(36, 40).readUint32BE();
+		packet.rxTimestamp = toMsecs(data, 32);
+
+		// const txTimestampHigh = data.slice(40, 44).readUint32BE();
+		// const txTimestampLow = data.slice(44, 48).readUint32BE();
+		packet.txTimestamp = toMsecs(data, 40);
+
+		return packet;
+	}
+
+	bufferize(packet) {
+		const buffer = Buffer.alloc(48).fill(0x00);
+
+		buffer[0] = (packet.leap << 6) | (packet.version << 3) | (this.mode << 0);
+		buffer[1] = packet.stratum;
+		buffer[2] = packet.poll;
+		buffer[3] = packet.precision;
+
+		buffer.writeUInt32BE(packet.rootDelay, 4);
+		buffer.writeUInt32BE(packet.rootDispersion, 8);
+		buffer.writeUInt32BE(packet.referenceId, 12);
+
+		// Reference Timestamp
+		writeInMillis(buffer, 16, packet.referenceTimestamp, true);
+
+		// Originate timestamp
+		writeInMillis(
+			buffer,
+			24,
+			packet.originateTimestamp,
+			this.mode !== MODES.SERVER // Don't add NTP_DELTA if the packet is server mode
+		);
+
+		// RX Timestamp
+		writeInMillis(buffer, 32, packet.rxTimestamp, true);
+
+		// TX Timestamp
+		writeInMillis(buffer, 40, packet.txTimestamp, true);
+
+		return buffer;
+	}
+}
+
+module.exports = { NTPPacket, sysToNTP, MODES };
