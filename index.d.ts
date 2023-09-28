e91341cf4571b64eef9dcda8b66bbfd38801dafd
Thiago Tognoli <tt.dev@gmail.com>
2023-09-28 13:05:55 -0300

    typing

--- index.d.ts
+++ index.d.ts
@@ -1,38 +1,38 @@
 export interface NTPPacket {
-	leapIndicator: number;
-	version: number;
-	mode: number;
-	stratum: number;
-	pollInterval: number;
-	precision: number;
-	referenceIdentifier: Buffer;
-	referenceTimestamp: number;
-	originateTimestamp: number;
-	receiveTimestamp: number;
-	transmitTimestamp: number;
-	rootDelay: Buffer;
-	rootDispersion: Buffer;
-	destinationTimestamp: number;
-	time: Date;
-	d: number;
-	t: number;
+  leapIndicator: number;
+  version: number;
+  mode: number;
+  stratum: number;
+  pollInterval: number;
+  precision: number;
+  referenceIdentifier: Buffer;
+  referenceTimestamp: number;
+  originateTimestamp: number;
+  receiveTimestamp: number;
+  transmitTimestamp: number;
+  rootDelay: Buffer;
+  rootDispersion: Buffer;
+  destinationTimestamp: number;
+  time: Date;
+  d: number;
+  t: number;
 }
 
 export interface NTPOptions {
-	timeout: number;
+  timeout: number;
 }
 
 declare class Client {
-	constructor(server?: string, port?: number, options?: NTPOptions);
-	syncTime(): Promise<NTPPacket>;
+  constructor(server?: string, port?: number, options?: NTPOptions);
+  syncTime(): Promise<NTPPacket>;
 }
 
 export interface ServerOptions {
-	onInvalidPacket?: (message: string, error: any) => any;
+  onInvalidPacket?: (message: string, error: any) => any;
 }
 
 declare class Server {
-	constructor(options?: ServerOptions);
-	handle(handler: Function): void;
-	listen(port?: number, callback?: () => void): Server;
+  constructor(options?: ServerOptions);
+  handle(handler: Function): void;
+  listen(port?: number, callback?: Function): Server;
 }
