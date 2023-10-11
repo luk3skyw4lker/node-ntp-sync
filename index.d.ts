62244277e66b5b244aa7e99c88d9b5624ece2ee7
Thiago Tognoli <thiagotognoli@users.noreply.github.com>
2023-10-11 14:43:03 -0300

    fix: close socket on error (#21)
    * feat: typing
    
    * Update package.json
    
    * feat: close socket on error
    
    * typing
    
    ---------
    
    Co-authored-by: Thiago Tognoli <tt.dev@gmail.com>

--- index.d.ts
+++ index.d.ts
@@ -1,33 +1,38 @@
 export interface NTPPacket {
-	leapIndicator: number;
-	version: number;
-	mode: number;
-	stratum: number;
-	pollInterval: number;
-	precision: number;
-	referenceIdentifier: Buffer;
-	referenceTimestamp: number;
-	originateTimestamp: number;
-	receiveTimestamp: number;
-	transmitTimestamp: number;
-	rootDelay: Buffer;
-	rootDispersion: Buffer;
-	destinationTimestamp: number;
-	time: Date;
-	d: number;
-	t: number;
+  leapIndicator: number;
+  version: number;
+  mode: number;
+  stratum: number;
+  pollInterval: number;
+  precision: number;
+  referenceIdentifier: Buffer;
+  referenceTimestamp: number;
+  originateTimestamp: number;
+  receiveTimestamp: number;
+  transmitTimestamp: number;
+  rootDelay: Buffer;
+  rootDispersion: Buffer;
+  destinationTimestamp: number;
+  time: Date;
+  d: number;
+  t: number;
 }
 
 export interface NTPOptions {
-	timeout: number;
+  timeout: number;
 }
 
 declare class Client {
-	constructor(server?: string, port?: number, options?: NTPOptions);
-	syncTime(): Promise<NTPPacket>;
+  constructor(server?: string, port?: number, options?: NTPOptions);
+  syncTime(): Promise<NTPPacket>;
+}
+
+export interface ServerOptions {
+  onInvalidPacket?: (message: string, error: any) => any;
 }
 
 declare class Server {
-	constructor();
-	handle(handler: Function): void;
+  constructor(options?: ServerOptions);
+  handle(handler: Function): void;
+  listen(port?: number, callback?: Function): Server;
 }
